b2febe17ff30bac86ca3bf2ca5d2fa27476ebc43
Noelle Kreidl <a12029871@unet.univie.ac.at>
2024-05-26 18:59:06 +0200

    Added data and two functions to change individual values in each layer or perturb all values in only one layer

--- pytorchfi/functions_for_testing.py
+++ pytorchfi/functions_for_testing.py
@@ -56,7 +56,14 @@ class custom_func(FaultInjection):
         int32_output = int32_output ^ bit_mask
 
         float_output = torch.from_numpy(int32_output.view(np.float32))
-        float_output = float_output.reshape(shape)
+        try:
+            # Reshape back to the original shape
+            float_output = float_output.reshape(shape)
+        except RuntimeError as e:
+            print(f"Error during reshape: {e}")
+            print(f"Expected shape: {shape}, Actual size: {float_output.numel()}")
+            print(f"Original output size: {output.numel()}, int32_output size: {int32_output.size}")
+            return
 
         if output.is_cuda :
             output.data.copy_(float_output.to(output.device))
@@ -76,7 +83,23 @@ class custom_func(FaultInjection):
             self.reset_current_layer()
 
     #multiply only one value by a factor
+    #see whats more vulnerable test multiple things: eg 1 value per layer fixed/random or all values for one layer 
     def multiply_value(self, module, input, output) :
+        output[self.index] = output * (1+self.epsilon)
+
+        self.update_layer()
+        if self.current_layer >= self.get_total_layers():
+            self.reset_current_layer()
+
+    def multiply_value_layer(self, module, input, output) :
+        if(self.index == self.current_layer) :
+            output[:] = output * (1+self.epsilon)
+
+        self.update_layer()
+        if self.current_layer >= self.get_total_layers():
+            self.reset_current_layer()
+
+    def multiply_one_value_layer(self, module, input, output) :
         output[self.index] = output[self.index] * (1+self.epsilon)
 
         self.update_layer()
